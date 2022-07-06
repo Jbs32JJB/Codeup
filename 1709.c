a1b50dcf870a4183daa763b11672ae0d0808aa1d
JongBin Jo <jbs32_1317@js.hs.kr>
2022-07-07 07:08:06 +0900

    Codeup Problem Num.1709

---
+++ 1709.c
@@ -0,0 +1,31 @@
+    #include <stdio.h>
+    int main()
+    {
+        int n;
+        int key;
+        int temp;
+        int a[100000];
+        scanf("%d",&n);
+        for(int i=0; i<n; i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(int k=1; k<n; k++)
+        {
+            key = a[k];
+            for(int j=k-1; j>=0; j--)
+            {
+                if(a[j]<key)
+                {
+                    temp = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = key;
+                }
+            }
+        }
+        for(int m=0; m<n; m++)
+        {
+            printf("%d\n",a[m]);
+        }
+        return 0;
+    }
