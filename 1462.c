ca5c54638c64e2d1c4374d07e935f3a3550c0437
JongBin Jo <jbs32_1317@js.hs.kr>
2022-07-14 13:54:22 +0900

    Codeup Problem Pack.1462 - 1467

---
+++ 1462.c
@@ -0,0 +1,24 @@
+    #include <stdio.h>
+    int main()
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n][n];
+        int x=1;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                a[j][i] = x++; // (n-1)*n+j
+            }
+        }
+        for(int l=0; l<n; l++)
+        {
+            for(int m=0; m<n; m++)
+            {
+                printf("%d ",a[l][m] );
+            }
+            printf("\n");
+        }
+        return 0;
+    }
---
+++ 1465.c
@@ -0,0 +1,24 @@
+    #include <stdio.h>
+    int main()
+    {
+        int n,m;
+        scanf("%d %d",&n,&m);
+        int a[n][m];
+        int x=1;
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=0; j<m; j++)
+            {
+                a[i][j] = x++; // (n-1)*n+j
+            }
+        }
+        for(int l=0; l<n; l++)
+        {
+            for(int k=0; k<m; k++)
+            {
+                printf("%d ",a[l][k] );
+            }
+            printf("\n");
+        }
+        return 0;
+    }
